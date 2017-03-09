c310ab769ecf5b242a864abaebca37526857f0cd
devdatta1 <devdatta.khadakban@sts.in>
2017-03-09 12:01:31 +0530

    new file
    new filre

---
+++ Specification.php
@@ -0,0 +1,11 @@
+
+Mobile technology ---
+Mobile technology is the technology used for cellular communication. 
+Mobile code division multiple access (CDMA) technology has evolved rapidly over the past few years. 
+Since the start of this millennium, a standard mobile device has gone from being no more than 
+a simple two-way pager to being a mobile phone, GPS navigation device, an embedded web browser 
+and instant messaging client, and a handheld game console. Many experts argue that the future 
+of computer technology rests in mobile computing with wireless networking. Mobile computing by 
+way of tablet computers are becoming more popular. Tablets are available on the 3G and 4G networks.
+
+
